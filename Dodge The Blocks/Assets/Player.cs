0ea303e286f3971d0f6dc6a221fa6c9c624f98e6
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-05 01:24:41 +0530

    Set up player movement, restricted the player on the horizontal axis

---
+++ Dodge The Blocks/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 15f;
+	public float mapWidth = 5f;
+
+	private Rigidbody2D _rb;
+
+	void Start()
+	{
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		Movement();
+	}
+
+	public void Movement()
+	{
+		float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed; //smoothened out value. Else use rawInput
+
+		Vector2 newPosition = _rb.position + Vector2.right * x;
+
+		newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth); //clamping the horizontal positions
+
+		_rb.MovePosition(newPosition);
+	}
+}
