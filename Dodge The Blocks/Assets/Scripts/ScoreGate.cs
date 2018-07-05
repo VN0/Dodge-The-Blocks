8856c9de6343e5a276cd0ce396be67963914c67f
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-06 00:44:25 +0530

    Implemented a crude method to keep a score count

---
+++ Dodge The Blocks/Assets/Scripts/ScoreGate.cs
@@ -0,0 +1,19 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreGate : MonoBehaviour
+{
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.GetComponent<Collider2D>().tag == "ScorePoint")
+		{
+			Debug.Log("Scored a point");
+		}
+	}
+}
